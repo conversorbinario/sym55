eca66347f3d986c155bfff4ed794af060daa05ca
Manuel <manuel.gon.pos@gmail.com>
2022-12-03 20:14:45 +0100

    Añado mail testing como sender

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('manuel.gon.pos@gmail.com', 'Manuel Bot'))
+                    ->from(new Address('testingmail@test.com', 'Manuel Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
